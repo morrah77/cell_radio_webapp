ab03710c05bfd759f8a84fe8d777772c3097a47c
Helen Lazar <helen.lazar.77@gmail.com>
2019-02-28 20:29:10 +0700

    Add environment params

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  GM_API_KEY: '',
+  basePath: 'ws://localhost:9091/ws/lastpoints'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  GM_API_KEY: '',
+  basePath: 'ws://localhost:8081/ws/lastpoints'
 };
 
 /*
